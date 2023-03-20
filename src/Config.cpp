d7076ffbb0511e5fdead9ddc79b3cb109d8810a3
Andrey Yarosh <bingo@bingosoft.info>
2023-03-20 22:28:08 +0100

    Update Config.cpp

--- src/Config.cpp
+++ src/Config.cpp
@@ -1,8 +1,8 @@
 #include "Config.h"
 
 Config Config::d1Mini = Config(
-    "bingosoft-wifi",
-    "159753Bingo",
+    "WIFI_SSID_NAME",
+    "YOUR_WIFI_PASSWORD",
     "142.132.210.78",
     13,
     14,
